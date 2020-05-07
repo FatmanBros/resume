226b547078017d79296c5986f3feafb0d243a0c9
takuma <takuma.watanabe@sola-air.com>
2020-05-07 22:47:29 +0900

    fix: 年、月のデータ構造を変更

--- src/app/resume-grid/resume-grid.component.ts
+++ src/app/resume-grid/resume-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injector, Input } from '@angular/core';
+import { Component, OnInit, Injector, Input, ElementRef } from '@angular/core';
 import { BaseComponent } from '../common/base/base.component';
 import { Resume } from '../model/resume';
 import { DateUtility } from '../utility/date-utility'
@@ -24,28 +24,81 @@ export class ResumeGridComponent extends BaseComponent {
   @Input()
   public termTo: Date = DateUtility.addMonths(new Date(), 6);
 
-  public term: { year: string, month: string }[];
+  /**
+   * グリッドの開始位置
+   */
+  public basePostion: number = 0;
+
+  /**
+   * 年リスト
+   */
+  public years: { year: string, count: number }[];
+
+  /**
+   * 月リスト
+   */
+  public month: { year: string, month: string, position: number }[];
 
   // TODO: 履歴のサーバー取得処理
   public resumes: Resume[] = [];
 
-  constructor(injector: Injector) {
+  /**
+   * セルのサイズ
+   */
+  public cellWidth: 19;
+
+  constructor(
+    injector: Injector,
+    private elementRef: ElementRef
+  ) {
     super(injector);
   }
 
   ngOnInit(): void {
+    // 年リスト作成
+    this.setYear();
+    // 月リスト作成
     this.setTerm();
   }
 
+  ngAfterViewInit() {
+    this.basePostion = this.elementRef.nativeElement.getBoundingClientRect();
+  }
+
+  /**
+   * 年リスト作成
+   */
+  private setYear() {
+    this.years = [];
+
+    let d = new Date(this.termFrom);
+    let end = DateUtility.getYearMonth(this.termTo);
+    let obj;
+    while (DateUtility.getYearMonth(d) !== end) {
+      if (!obj || obj.year !== d.getFullYear()) {
+        obj = { year: d.getFullYear(), count: 0 };
+        this.years.push(obj);
+      }
+      // カウントアップ
+      obj.count += 1;
+      d = DateUtility.addMonths(d, 1);
+    }
+  }
+
+  /**
+   * 月リスト作成
+   */
   private setTerm() {
     let d = new Date(this.termFrom);
     let end = DateUtility.getYearMonth(this.termTo);
 
-    this.term = [];
+    this.month = [];
+    let i = 0;
     while (DateUtility.getYearMonth(d) !== end) {
-      this.term.push({
+      this.month.push({
         year: '' + d.getFullYear(),
         month: '' + (d.getMonth() + 1),
+        position: this.cellWidth * i,
       })
       d = DateUtility.addMonths(d, 1);
     }
